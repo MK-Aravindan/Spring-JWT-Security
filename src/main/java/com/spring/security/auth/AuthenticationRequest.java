3e3d2dc7dcb8e748bdb3080b6a583e030ce3f929
MK-Aravindan <aravindanmk02@gmail.com>
2023-03-14 15:59:56 +0530

    first commit

---
+++ src/main/java/com/spring/security/auth/AuthenticationRequest.java
@@ -0,0 +1,18 @@
+package com.spring.security.auth;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+  @Column(unique = true)
+  private String email;
+  String password;
+}
